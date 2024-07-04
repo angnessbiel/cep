ada7e8568f50ca4216fff1fbef34e3d5477aeda8
angnessbiel <131162584+angnessbiel@users.noreply.github.com>
2024-07-04 08:04:18 -0300

    e

--- src/components/BuscarCep.jsx
+++ src/components/BuscarCep.jsx
@@ -1,44 +1,52 @@
 import { useState } from "react";
 
 export default function BuscarCep(){
-    const [cep, setCep] = useState('')
-    const [endereco, setEndereco] = useState(null)
-    const [erro, setErro] = useState(null)
+    const [cep, setCep] = useState('');
+    const [endereco, setEndereco] = useState(null);
+    const [erro, setErro] = useState(null);
 
     const fetchData = async () => {
-        try{
-            const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-            const data = await response.json()
-            setEndereco(data)
-        } catch (error){
-            console.error(error)
+        setErro(null);
+        setEndereco(null);
+
+        try {
+            const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+            const data = await response.json();
+
+            if (data.erro) {
+                setErro('CEP não encontrado.');
+            } else {
+                setEndereco(data);
+            }
+        } catch (error) {
+            setErro('Erro ao buscar CEP.');
+            console.error(error);
         }
-    }
-    return(
+    };
+
+    return (
         <div className="content">
             <h1>Buscar endereço por CEP</h1>
             <input
-            type="text"
-            value={cep}
-            placeholder="Digite aqui"
-            onChange={(e) => setCep(e.target.value)}
-            ></input>
+                type="text"
+                value={cep}
+                placeholder="Digite aqui"
+                onChange={(e) => setCep(e.target.value)}
+            />
 
             <button onClick={fetchData}>Buscar</button>
-            {erro && (
-                <p>CEP não encontrado.</p>
-            )}
-        {endereco && (
-            <div className="endereco">
-                <h2>Endereço</h2>
-                <p>Rua: {endereco.logradouro}</p>
-                <p>Bairro: {endereco.bairro}</p>
-                <p>Cidade: {endereco.localidade}</p>
-                <p>UF: {endereco.uf}</p>
-            </div>
 
+            {erro && <p>{erro}</p>}
 
+            {endereco && (
+                <div className="endereco">
+                    <h2>Endereço</h2>
+                    <p>Rua: {endereco.logradouro}</p>
+                    <p>Bairro: {endereco.bairro}</p>
+                    <p>Cidade: {endereco.localidade}</p>
+                    <p>UF: {endereco.uf}</p>
+                </div>
             )}
         </div>
-    )
+    );
 }
